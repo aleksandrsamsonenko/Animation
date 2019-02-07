1b674c912aae5241a77a1d3b262424172278f635
aleksandr_samsonenko <aleksandrsamsonenko0794@gmail.com>
2019-02-08 01:26:49 +0200

    First Commit

---
+++ dev/js/main.js
@@ -0,0 +1,6 @@
+$(function () {
+    setTimeout(function () {
+        $('[data-js=header]').addClass('-white');
+        $('[data-js=hero]').addClass('-hide');
+    },5000)
+});
---
+++ dev/js/scripts.js
@@ -0,0 +1,8 @@
+module.exports = {
+    libs: [
+        "node_modules/jquery/dist/jquery.js"
+    ],
+    main: [
+        "dev/js/main.js",
+    ]
+};
